86f54d70c9a295bd1f44fa660642ce649f71ea5f
kuolematon <sappuku@gmail.com>
2025-01-30 02:28:24 +0200

    update terraform

--- first_sprint/terraform/main.tf
+++ first_sprint/terraform/main.tf
@@ -2,15 +2,22 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "6.17.0"
+      version = "6.18.1"
     }
   }
 }
 
-resource "google_storage_bucket" "auto-expire" {
-  name          = "auto-expiring-bucket"
-  location      = "US"
+provider "google" {
+  project     = "zoomcamp-6887"
+  region      = "europe-north1"
+  zone        = "europe-north1-c"
+}
+
+resource "google_storage_bucket" "zoomc-ucket" {
+  name          = "zoomcamp-6887-terraform"
+  location      = "EU"
   force_destroy = true
+
 
   lifecycle_rule {
     condition {
